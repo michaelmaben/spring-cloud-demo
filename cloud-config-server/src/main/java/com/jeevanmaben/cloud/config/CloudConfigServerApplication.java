82798854f1f7805f6f950cfaaeec08520c538983
Jeevan Maben <jeevanmaben@Jeevans-MacBook-Pro.local>
2021-09-21 11:49:19 +0530

    add common properties to cloud config

---
+++ cloud-config-server/src/main/java/com/jeevanmaben/cloud/config/CloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.jeevanmaben.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ common-cloud-config-server/README.md
@@ -0,0 +1,1 @@
+# common-cloud-config-server
