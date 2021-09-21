9b501708d5a8465f36a73d435143e6ac8d9348f9
Jeevan Maben <jeevanmaben@Jeevans-MacBook-Pro.local>
2021-09-21 19:57:54 +0530

    test

--- cloud-config-server/src/main/java/com/jeevanmaben/cloud/config/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/jeevanmaben/cloud/config/CloudConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.jeevanmaben.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigServerApplication {
 
 	public static void main(String[] args) {
